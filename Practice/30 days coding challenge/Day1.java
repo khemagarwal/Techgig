af4a49e414c5586e0c1a52bb62ec8752707e8ec9
khemagarwal <34685198+khemagarwal@users.noreply.github.com>
2020-04-15 16:30:05 +0530

    Create Day1.java

---
+++ Practice/30 days coding challenge/Day1.java
@@ -0,0 +1,18 @@
+/* 
+ * Enter your code here. Read input from STDIN. Print your output to STDOUT. 
+ * Your class should be named CandidateCode.
+*/
+
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+    public static void main(String args[] ) throws Exception {
+
+    	//Write code here
+        Scanner sc = new Scanner (System.in);
+        String name=sc.nextLine();
+        System.out.println("Hello Techgig");
+        System.out.print(name);
+
+   }
+}
